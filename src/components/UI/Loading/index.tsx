f0cabefe2a70211a97ad8b8c3d790085d1caadda
heronoa <heron.amaral@gmail.com>
2023-11-03 21:19:54 -0300

    feat: componente de loading

---
+++ src/components/UI/Loading/index.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="loading-circle"></div>
+    </div>
+  );
+};
+
+export default Loading
