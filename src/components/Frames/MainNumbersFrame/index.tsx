640709e37550039c0eb89d6b5ec5894878e43b8c
heronoa <heron.amaral@gmail.com>
2023-11-04 19:19:48 -0300

    feat: cirando primeiro frame do painel com o numero total de usuarios projetos e gestores

---
+++ src/components/Frames/MainNumbersFrame/index.tsx
@@ -0,0 +1,49 @@
+import { FaUserAlt, FaUserTie } from "react-icons/fa";
+import { AiOutlineProject } from "react-icons/ai";
+import { useContext } from "react";
+import { useProjects } from "@/hooks/useProjects";
+import { useUsers } from "@/hooks/useUsers";
+
+const MainNumbersFrame = () => {
+  const { allUsers } = useUsers();
+  const { allProjects } = useProjects();
+
+  return (
+    <div className="flex flex-col md:flex-row mx-auto w-[90%] justify-center items-center shadow-lg">
+      {[
+        {
+          icon: ({ className }: any) => <FaUserAlt className={className} />,
+          title: "Colaboradores",
+          number: allUsers.length,
+        },
+        {
+          icon: ({ className }: any) => <FaUserTie className={className} />,
+          title: "Gestores",
+          number: allUsers.filter(e => +e.permissionLevel > 1).length,
+        },
+        {
+          icon: ({ className }: any) => <AiOutlineProject className={className} />,
+          title: "Projetos",
+          number: allProjects.length,
+        },
+      ].map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="w-[300px] h-[300px] flex flex-col justify-center items-center"
+          >
+            <div className="w-36 h-36 border border-black dark:border-white rounded-full flex justify-center items-center">
+              {item.icon({ className: "w-24 h-24" })}
+            </div>
+            <div className="flex flex-col justify-center items-center mt-4">
+              <div className="text-[26px]">Colaboradores</div>
+              <div className="text-[32px]">{allUsers.length}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MainNumbersFrame;
--- src/components/Layouts/DashboardLayout/index.tsx
+++ src/components/Layouts/DashboardLayout/index.tsx
@@ -1,5 +1,15 @@
+import MainNumbersFrame from "@/components/Frames/MainNumbersFrame";
+
 const DashboardLayout = () => {
-    return <div></div>;
-}
+
+  return (
+    <section className="flex items-center flex-col dark:text-white shadow-2xl min-h-[80vh] m-4">
+      <h3>Painel</h3>
+      <div className="w-full flex flex-col justify-center mt-12">
+        <MainNumbersFrame />
+      </div>
+    </section>
+  );
+};
 
 export default DashboardLayout;
