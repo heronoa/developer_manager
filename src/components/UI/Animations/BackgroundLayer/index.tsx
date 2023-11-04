71fd58c2540863477e5b190282d7dbc55150dc3b
heronoa <heron.amaral@gmail.com>
2023-11-03 21:21:18 -0300

    feat: componentes de animação lateral e fade

---
+++ src/components/UI/Animations/BackgroundLayer/index.tsx
@@ -0,0 +1,16 @@
+import { Transition } from "@headlessui/react";
+
+const BackgroundLayer = () => (
+  <Transition.Child
+    enter="transition-opacity ease-in-out duration-500"
+    enterFrom="opacity-0"
+    enterTo="opacity-100"
+    leave="transition-opacity ease-in-out duration-500"
+    leaveFrom="opacity-100"
+    leaveTo="opacity-0"
+  >
+    <div className="fixed inset-0 bg-gray-500 opacity-75" />
+  </Transition.Child>
+);
+
+export default BackgroundLayer;
---
+++ src/components/UI/Animations/SlideOverLayer/index.tsx
@@ -0,0 +1,32 @@
+import { Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const SlideOverLayer = ({ children }: Props) => (
+  <Transition.Child
+    as={Fragment}
+    enter="transform transition ease-in-out duration-500"
+    enterFrom="-translate-x-full"
+    enterTo="translate-x-0"
+    leave="transform transition ease-in-out duration-500 delay-100"
+    leaveFrom="translate-x-0"
+    leaveTo="-translate-x-full"
+  >
+    <div className="fixed inset-0 overflow-hidden">
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="pointer-events-none fixed inset-y-0 left-0 flex max-w-full pr-10">
+          <div className="pointer-events-auto w-screen max-w-2xl">
+            <div className="flex h-full flex-col overflow-y-scroll bg-white dark:bg-[#333333] py-6 shadow-xl">
+              <div className="px-4 sm:px-6">{children}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Transition.Child>
+);
+
+export default SlideOverLayer;
