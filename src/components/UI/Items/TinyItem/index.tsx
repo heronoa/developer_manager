906a0dd3e59f9c83352a5ee3fc971848bb89afc8
heronoa <heron.amaral@gmail.com>
2023-11-05 01:31:22 -0300

    feat: criando possiveis stacks e atuacoes e suas cores relacionadas

--- src/components/UI/Items/TinyItem/index.tsx
+++ src/components/UI/Items/TinyItem/index.tsx
@@ -1,9 +1,26 @@
+import { capitalize } from "@/services/format";
+import { possibleOccupations, possiblesStacks } from "@/utils/constants";
+
 interface Props {
-    value: string
+  value: string;
 }
 
 const TinyItem = ({ value }: Props) => {
-  return <div className="bg-yellow-600 text-white rounded-full p-2 w-fit text-center">{value}</div>;
+  const color =
+    possiblesStacks?.[capitalize(value) as keyof typeof possiblesStacks] ||
+    possibleOccupations?.[
+      value.toLocaleLowerCase() as keyof typeof possibleOccupations
+    ] ||
+    "#a40100";
+
+  return (
+    <div
+      style={{ background: color }}
+      className="text-white rounded-full p-2 w-fit text-center"
+    >
+      {value}
+    </div>
+  );
 };
 
-export default TinyItem
+export default TinyItem;
