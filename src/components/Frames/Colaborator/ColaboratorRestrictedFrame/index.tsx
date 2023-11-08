594b6163028e527e359a687854e722473d97c520
heronoa <heron.amaral@gmail.com>
2023-11-08 10:00:42 -0300

    feat: implementando edicao de dados restritos da pagina de usuario

--- src/components/Frames/Colaborator/ColaboratorRestrictedFrame/index.tsx
+++ src/components/Frames/Colaborator/ColaboratorRestrictedFrame/index.tsx
@@ -1,8 +1,12 @@
 import { IRestrictedDataType, IUserDataType } from "@/@types";
+import EditButton from "@/components/Auth/EditButton";
+import EdittableListItems from "@/components/UI/Items/EdittableListItems";
 import TinyItem from "@/components/UI/Items/TinyItem";
 import { useUsers } from "@/hooks/useUsers";
 import { translateItemKeys, formatItem } from "@/services/format";
 import { useEffect, useState } from "react";
+import { GiConfirmed } from "react-icons/gi";
+import { ImCancelCircle } from "react-icons/im";
 
 interface Props {
   user: IUserDataType;
@@ -22,25 +26,110 @@ const ColaboratorRestrictedFrame = ({ user }: Props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const { updateUser } = useUsers();
+
+  const [workType, setWorkType] = useState<string | undefined>();
+  const [edittables, setEdittables] = useState<
+    Partial<IUserDataType & IRestrictedDataType>
+  >({});
+
+  const handleChangeEdittables = (
+    key: keyof (IUserDataType & IRestrictedDataType),
+    value: string | undefined,
+  ) => {
+    setEdittables(prevState => {
+      const newState = JSON.parse(JSON.stringify(prevState));
+      newState[key] = value;
+      return newState;
+    });
+  };
+
+  const submitEdittable =
+    (key: keyof (IUserDataType & IRestrictedDataType)) => async () => {
+      const obj: any = { uid: user.uid };
+      obj[key] = edittables[key as keyof (IUserDataType & IRestrictedDataType)];
+      await updateUser(obj);
+      handleChangeEdittables(key, undefined);
+    };
+
   return (
     <div className="frame-container">
       <div className="grid md:grid-cols-2 grid-cols-1 justify-evenly gap-4 w-full mt-4">
         {data &&
-          Object.entries(data).map(([objKey, objValue], index) => {
-            if (["uid", "projects", "occupation"].includes(objKey)) return null;
+          Object.entries({
+            telefone: data.telefone,
+            cpf: data.cpf,
+            rg: data.rg,
+            workType: data.workType,
+          }).map(([objKey, objValue], index) => {
+            const typeKey: keyof Partial<IUserDataType> =
+              objKey as keyof Partial<IUserDataType>;
+
+            if (objKey === "workType") {
+              return (
+                <EdittableListItems
+                  key={index}
+                  state={workType}
+                  setState={setWorkType}
+                  objEntries={[objKey, [objValue]]}
+                  submit={submitEdittable(objKey)}
+                />
+              );
+            }
 
             return (
-              <div key={index}>
-                <span className="font-semibold mr-2">
+              <div
+                key={`${user.uid.slice(0, 12)}-${objKey}-${objValue}-${index}`}
+              >
+                <span className="text-[21px] font-semibold mr-2">
                   {translateItemKeys(objKey as any)}:
                 </span>
-                <div className="flex flex-wrap">
-                  {objKey === "occupation"
-                    ? objValue.map((e: any, index: any) => {
-                        return <TinyItem key={index} value={e} />;
-                      })
-                    : formatItem(objValue, objKey as any)}
-                </div>
+
+                {!edittables?.[typeKey] && (
+                  <div className="text-[21px] w-[40%] relative">
+                    <div className="flex flex-wrap">
+                      {formatItem(objValue, objKey as any)}
+                    </div>
+                    <div className="absolute top-1 -right-8">
+                      <EditButton
+                        fn={() =>
+                          handleChangeEdittables(typeKey, objValue ?? undefined)
+                        }
+                      />
+                    </div>
+                  </div>
+                )}
+                {edittables?.[typeKey] && (
+                  <div className="relative w-[60%]">
+                    <input
+                      className="text-[21px] w-full bg-transparent"
+                      type={typeKey === "birthday" ? "date" : typeKey}
+                      value={
+                        typeKey === "birthday"
+                          ? (formatItem(objValue, "birthday")
+                              ?.split("/")
+                              ?.reverse()
+                              ?.join("-") as string) ?? ""
+                          : edittables?.[typeKey] || (user?.[typeKey] ?? "")
+                      }
+                      onChange={evt =>
+                        handleChangeEdittables(typeKey, evt.target.value)
+                      }
+                    />
+                    <div className="absolute flex top-1 gap-2 -right-20 ">
+                      <GiConfirmed
+                        className="w-8 h-8 cursor-pointer"
+                        onClick={() => submitEdittable(typeKey)()}
+                      />
+                      <ImCancelCircle
+                        className="w-8 h-8 cursor-pointer"
+                        onClick={() =>
+                          handleChangeEdittables(typeKey, undefined)
+                        }
+                      />
+                    </div>
+                  </div>
+                )}
               </div>
             );
           })}
