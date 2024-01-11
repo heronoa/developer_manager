0bf08e89a66446782cca6aa709b46815698f3868
heronoa <heron.amaral@gmail.com>
2024-01-11 19:35:36 -0300

    style: minor changes

--- src/components/UI/CompanyLogo/index.tsx
+++ src/components/UI/CompanyLogo/index.tsx
@@ -1,25 +1,23 @@
-import Image from "next/image";
+import { SiPhpmyadmin } from "react-icons/si";
 
 interface Props {
   className?: string;
-  label?:boolean;
+  label?: boolean;
 }
 
-const CompanyLogo = ({ className = "", label=true }: Props) => {
+const CompanyLogo = ({ className = "", label = true }: Props) => {
   return (
-    <div className={`${className} flex gap-4 items-center text-blue-900 hover:text-blue-800 cursor-pointer transition duration-150 mx-2 mt-6 mb-6 `}>
-      <Image
-        className=""
-        src={"/imgs/logo.png"}
-        alt={"Logo da Encibra SA"}
-        width={90}
-        height={56}
-      />
-      {label && <h1 className="text-base h-[70px] flex flex-col justify-end dark:text-white">
-        Encibra SA
-        <br />
-        <small className="tiny">Estudos e Projetos de Engenharia</small>
-      </h1>}
+    <div
+      className={`${className} dark:text-white flex gap-4 items-center text-blue-900 hover:text-blue-800 cursor-pointer transition duration-150 mx-2 mt-6 mb-6 `}
+    >
+      <SiPhpmyadmin className="w-[90px] h-[56px]" alt={"Company Logo"} />
+      {label && (
+        <h1 className="text-base h-[70px] flex flex-col justify-end dark:text-white">
+          Dev Manager
+          <br />
+          <small className="tiny">Manage as a captain</small>
+        </h1>
+      )}
     </div>
   );
 };
